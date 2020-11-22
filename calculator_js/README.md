a43b543de056051ced8426102af780dec9db7dfa
tush-tr <iamtush-tr@DESKTOP-T3039R4.localdomain>
2020-11-23 00:00:52 +0530

    Making a web calculator using vanilla javascript

---
+++ calculator_js/README.md
@@ -0,0 +1,3 @@
+# calculator_js
+Making a calculator using vanilla js
+<img src="image.png">
---
+++ calculator_js/script.js
@@ -0,0 +1,8 @@
+console.log("hello");
+function one_num(val){
+    console.log('hello one');
+    document.getElementById('screen').value += val;
+}
+const do_null = ()=>{
+    document.getElementById('screen').innerHTML = null;
+}
