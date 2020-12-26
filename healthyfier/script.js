83021c9b1a435739c1ac340d827cf8fca4d6a599
tush-tr <iamtush-tr@DESKTOP-T3039R4.localdomain>
2020-12-26 08:26:17 +0530

    bmr and tdee calculator

--- healthyfier/script.js
+++ healthyfier/script.js
@@ -3,21 +3,45 @@ var gender = document.querySelector("#gender");
 var weight = document.querySelector("#weight");
 var age = document.querySelector("#age");
 var height = document.querySelector("#height");
-var show = document.querySelector(".showcase");
-
-
+var showbmr = document.querySelector(".showbmr");
+var lifestyle = document.querySelector("#lifestyle");
+var showtdee = document.querySelector(".showtdee");
 
 button.addEventListener("click", () => {
+  let ls = lifestyle.value;
   let gen = gender.value;
   let wei = Number(weight.value);
   let ag = Number(age.value);
   let hei = Number(height.value);
-  if(gen=="male"){
-  let bmr = 66.47 + (13.75 * wei) + (5.003 * hei) - (6.755 * ag);
-  show.innerHTML = `<h1>Your BMR is ${bmr}</h1>`;
+  if (gen == "male") {
+    let bmr = (10 * wei) + (6.25 * hei) - (5 * ag) + 5;
+    showbmr.innerHTML = `<h1>Your BMR is ${bmr}</h1>`;
+    if (ls == 1){
+      showtdee.innerHTML = `<h1>Your TDEE is ${bmr * 1.5}</h1>`;
+    }
+    else if(ls == 2){
+      showtdee.innerHTML = `<h1>Your TDEE is ${bmr * 1.8}</h1>`;
+
+    }
+    else {
+      showtdee.innerHTML = `<h1>Your TDEE is ${bmr * 2.2}</h1>`;
+    }
   }
-  else{
-    let bm = 655.1 + (9.563*wei) + (1.85*hei) - (4.676*ag);
-    show.innerHTML = `<h1>Your BMR is ${bm}</h1>`;
+  else {
+    let bm = (10 * wei) + (6.25 * hei) - (5 * ag) - 161;
+    showbmr.innerHTML = `<h1>Your BMR is ${bm}</h1>`;
+    if (ls == 1){
+      showtdee.innerHTML = `<h1>Your TDEE is ${bm * 1.5}</h1>`;
+    }
+    else if(ls == 2){
+      showtdee.innerHTML = `<h1>Your TDEE is ${bm * 1.8}</h1>`;
+
+    }
+    else {
+      showtdee.innerHTML = `<h1>Your TDEE is ${bm * 2.2}</h1>`;
+    }
+
+
   }
+
 })
